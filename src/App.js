d377419df19e51668e01f95d19bbef55d3986f3e
rajesh-govada <govadarajesh+fungit@gmail.com>
2025-04-02 23:47:28 +0530

    with love

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         Many More Happy Returns of the Day 
+         <p>My Dear sweet Wifey 😘!</p>
+         <p>A special day to our wonderful beginning</p>
+         <p>The start of our beautiful Journey ❤️</p>
+        </p>                   
       </header>
     </div>
   );
